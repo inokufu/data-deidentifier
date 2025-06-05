af8a4653d663ca7118d9cc414521789fc07ffaed
pierrocknroll <pierrock+github@protonmail.com>
2025-06-05 16:10:47 +0200

    :twisted_rightwards_arrows: Fix merge

--- src/data_deidentifier/adapters/api/mapper.py
+++ src/data_deidentifier/adapters/api/mapper.py
@@ -28,6 +28,7 @@ class ApiEntityMapper(EntityMapperContract[EntityResponse]):
             end=entity.end,
             score=entity.score,
             text=entity.text,
+            path=entity.path,
         )
 
     @staticmethod
@@ -39,6 +40,7 @@ class ApiEntityMapper(EntityMapperContract[EntityResponse]):
             end=entity_response.end,
             score=entity_response.score,
             text=entity_response.text,
+            path=entity_response.path,
         )
 
 
--- src/data_deidentifier/adapters/api/response.py
+++ src/data_deidentifier/adapters/api/response.py
@@ -24,6 +24,10 @@ class EntityResponse(BaseModel):
         description="The confidence score of the detection",
     )
     text: str | None = Field(default=None, description="The text of the entity")
+    path: str | None = Field(
+        None,
+        description="The path to the field containing the entity (for JSON data)",
+    )
 
 
 class StructuredFieldResponse(BaseModel):
