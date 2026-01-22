2e8c2fdd0cd7c84845c98ddb2909008a7d6db578
guilfb <guillaumelefebvre69@gmail.com>
2026-01-22 12:08:57 +0100

    :test_tube: remove temporary middleware

--- src/data_deidentifier/adapters/api/main.py
+++ src/data_deidentifier/adapters/api/main.py
@@ -1,11 +1,8 @@
-from collections.abc import AsyncIterator, Callable
+from collections.abc import AsyncIterator
 from contextlib import asynccontextmanager
-from http import HTTPStatus
-from logging import DEBUG, getLevelName, getLogger
-from time import time
 from typing import Any
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from logger import LogLevel, LoguruLogger
 
 from data_deidentifier.adapters.infrastructure.config.settings import Settings
@@ -59,47 +56,3 @@ exception_handler.configure(app=app)
 app.include_router(router=anonymize_router)
 app.include_router(router=pseudonymize_router)
 app.include_router(router=infra_router)
-
-# NOTE: Middleware to delete after performance testing
-# Disable uvicorn access logger
-uvicorn_access = getLogger("uvicorn.access")
-uvicorn_access.disabled = True
-
-logger = getLogger("uvicorn")
-logger.setLevel(getLevelName(DEBUG))
-
-
-async def log_request_middleware(request: Request, call_next: Callable) -> Any:  # noqa: ANN401
-    """Log request Middleware.
-
-    Args:
-        request (Request): The incoming request.
-        call_next (_type_): The next middleware or route handler to call
-
-    Returns:
-        Any: The response from the next middleware or route handler.
-    """
-    logger.debug("middleware: log_request_middleware")
-    url = (
-        f"{request.url.path}?{request.query_params}"
-        if request.query_params
-        else request.url.path
-    )
-    start_time = time()
-    response = await call_next(request)
-    process_time = (time() - start_time) * 1000
-    fmt_duration = f"{process_time:.2f}"
-    host = getattr(getattr(request, "client", None), "host", None)
-    port = getattr(getattr(request, "client", None), "port", None)
-    status = response.status_code
-    method = request.method
-    try:
-        phrase = HTTPStatus(status).phrase
-    except ValueError:
-        phrase = ""
-    msg = f'{host}:{port} - "{method} {url}" {status} {phrase} {fmt_duration}ms'
-    logger.debug(msg)
-    return response
-
-
-app.middleware("http")(log_request_middleware)
